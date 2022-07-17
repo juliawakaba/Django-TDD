c234b3d4b630d7d5a9e60b5c602144041df0f8c1
jmugure95 <jmugure95@gmail.com>
2022-07-17 23:31:14 +0300

    add wait for db file tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -26,4 +26,3 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-
