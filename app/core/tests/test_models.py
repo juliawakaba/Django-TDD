f4c95cfca62b105310814e8088992db0398929ee
jmugure95 <jmugure95@gmail.com>
2022-07-18 23:38:04 +0300

    cleaning up to fit flake8 stds

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,7 +25,7 @@ class ModelTests(TestCase):
             ['test2@example.COM', 'test2@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email,'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
