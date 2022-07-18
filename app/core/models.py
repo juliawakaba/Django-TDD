7d1cf36d2a70e3b19303e5fc43c47429533657c6
jmugure95 <jmugure95@gmail.com>
2022-07-18 22:08:54 +0300

    create superuser test and function

--- app/core/models.py
+++ app/core/models.py
@@ -7,11 +7,20 @@ class UserManager(BaseUserManager):
     """Manager for users"""
     def create_user(self, email, password=None, **extra_fields):
         """Create, svae and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
- 
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
