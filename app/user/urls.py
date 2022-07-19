45de0824677917375056a72f6aae63bc13158cd3
jmugure95 <jmugure95@gmail.com>
2022-07-19 13:46:14 +0300

    added token authentication and authentication tests

--- app/user/urls.py
+++ app/user/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
     path('me/', ManageUserView.as_view(), name='me'),
-
 ]
